8582c746c0d5b8f80430e968244c9e91dc289d23
Jeremy <jeremysabina@gmail.com>
2022-07-07 02:17:29 -0400

    Swiper needs fixing

--- js/main.js
+++ js/main.js
@@ -100,7 +100,20 @@ modalCloses.forEach((modalClose) => {
     })
 })
 
+// Swiper JavaScript //
 
+let swiper = new Swiper("..mySwiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
 
 // const loadText = document.querySelector('.loading-text');
